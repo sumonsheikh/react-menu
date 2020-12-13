ed2d9e453ce4c123a6d249db4c28b611514cfb3d
sumonsheikh <sm.sumon117@gmail.com>
2020-12-13 14:06:46 +0600

    fetch all menu item data from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, {useState} from 'react';
+
+//Api
 import data from './data';
+
+//Componen
 import Menu from './Menu';
+import Categories from './Categories';
 
 function App() {
-  const [items, setItems] = useState(data);
+  const [menuItems, setMenuItems] = useState(data);
+  const [categories, setCategories] = useState([]);
   return (
-    <div className="App">
-      <Menu items={items}/>
-    </div>
+    <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories/>
+        <Menu items={menuItems}/>
+    </section>
   );
 }
 
--- src/Menu.js
+++ src/Menu.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import SingleMenu from './SingleMenu';
+
 
 const Menu = ({items}) =>{
     
     return (
-        <article>
-            {items.map((item) =>{
+        <div className="section-center">
+            {items.map((menuItem) =>{
+                const{id,title,img,price,desc} = menuItem;
                 return(
-                    <SingleMenu key={item.id} {...item}/>
-                );
+                    <article key={id} className="menu-item">
+                        <img src={img} alt={title} className="photo"/>
+                        <header className="item-ifo">
+                            <h4 >{title}</h4>
+                            <h4 className="item-price">${price}</h4>
+                        </header>
+                        <p className="item-text">{desc}</p>
+                    </article>
+                )
             })}
-        </article>
-    )
+        </div>
+    );
 }
 export default Menu;
